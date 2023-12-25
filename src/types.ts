cf55e4ea6e5323df55d7e2d605dd59cc2d9f2bea
iambnlvn <iambnlvn@gmail.com>
2023-12-24 19:51:31 -0600

    Add new token types and update AST and expression types

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,29 @@
+export type TokenType =
+  | "NUMBER"
+  | "STRING"
+  | "OPERATOR"
+  | "IDENTIFIER"
+  | "LPAREN"
+  | "RPAREN"
+  | "SEMICOLON"
+  | "LBRACE"
+  | "RBRACE"
+  | "ADD_OP"
+  | "MUL_OP"
+  | "MOD_OP";
+
+export type Token = {
+  type: TokenType;
+  value: string;
+};
+
 export type Statement = ExpressionStatement | BlockStatement | EmptyStatement;
 export type EmptyStatement = {
   type: "EmptyStatement";
 };
 export type ExpressionStatement = {
   type: "ExpressionStatement";
-  expression: ASTNode<any>;
+  expression: ASTNode;
 };
 export type BlockStatement = {
   type: "BlockStatement";
@@ -14,11 +33,24 @@ export type Program = {
   type: "Program";
   body: Statement[];
 };
-export type AST<T> = {
+export type AST = {
   type: string;
-  body: { type: string; expression?: ASTNode<T> }[];
+  body: { type: string; expression?: ASTNode }[];
 };
-export type ASTNode<T> = {
-  type: string;
-  value?: T;
-};
+export type ASTNode =
+  | { type: "NumericLiteral"; value: number }
+  | { type: "StringLiteral"; value: string }
+  | {
+      type: "BinaryExpression";
+      operator: string;
+      left: ASTNode;
+      right: ASTNode;
+    }
+  | { type: "EmptyStatement" };
+
+export type ExpressionType =
+  | "PrimaryExpression"
+  | "MulExpression"
+  | "ModExpression";
+
+export type Operator = "ADD_OP" | "MUL_OP" | "MOD_OP";
